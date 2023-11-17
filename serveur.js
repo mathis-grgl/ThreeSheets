3f051894dc52cfc75cad24f8b721343d7d86fdcd
Boubix88 <86961375+Boubix88@users.noreply.github.com>
2023-11-17 11:24:27 +0100

    Serveur de base pour tester

--- serveur.js
+++ serveur.js
@@ -1,51 +1,14 @@
-import { stat, readdir, createReadStream } from "fs";
 import { createServer } from "http";
-import pkg from 'mime';
-const { getType } = pkg;
 
+let directory = process.cwd() + "/directory/";
 let server = createServer((request, response) => { // paramétrage
-    if (request.method !== 'GET') {
-        response.writeHead(405);
-        response.end('Method Not Allowed');
-        return;
-    }
-
-    let chemin = process.cwd() + request.url;
-    stat(chemin, (error, stats) => {
-        if (error) {
-            response.writeHead(404);
-            response.end('Pas de fichier trouvé');
-        } else {
-            if (stats.isFile()) {
-                console.log("c'est un fichier");
-                let header = getType(chemin);
-                response.setHeader('Content-Type', getType(header));
-                let stream = createReadStream(chemin);
-                stream.pipe(response);
-            }
-            if (stats.isDirectory()) {
-                console.log("c'est un dossier");
-                readdir(chemin, (error, files) => {
-                    if (error) {
-                        response.writeHead(500);
-                        response.end('Internal Server Error');
-                        return;
-                    }
-
-                    response.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
-                    response.write("<hl>Liste des fichiers dans le dossier</hl>");
-                    response.write("<ul>");
-                    files.forEach(file => {
-                        console.log("chemin fichier : " + chemin + file);
-                        response.write("<li> <a href=\"" + chemin + file + "\"> " + file + "</a> </li>");
-                    });
-                    response.write("</ul>");
-                    response.end();
-                });
-            }
-        }
-    });
+    response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    response.write(`
+<h1>Félicitations !</h1>
+<p>Vous venez de créer votre premier serveur.</p>
+<p>Vous cherchiez à accéder à la ressource <code>${directory + request.url}</code> en
+utilisant la méthode <code>${request.method}</code>.</p>`);
+    response.end();
 });
 server.listen(8500); // start !
 console.log("Serveur lancé.");
-
