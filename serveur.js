cbdeeb2bf243582f0b232b87affc315ed46d5ca4
mathis-grgl <76184877+mathis-grgl@users.noreply.github.com>
2023-11-17 11:33:38 +0100

    Basic index.html

--- serveur.js
+++ serveur.js
@@ -1,13 +1,9 @@
 import { createServer } from "http";
 
-let directory = process.cwd() + "/directory/";
 let server = createServer((request, response) => { // paramétrage
-    response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    response.write(`
-<h1>Félicitations !</h1>
-<p>Vous venez de créer votre premier serveur.</p>
-<p>Vous cherchiez à accéder à la ressource <code>${directory + request.url}</code> en
-utilisant la méthode <code>${request.method}</code>.</p>`);
+    // redirect to index.html
+    response.writeHead(302, {  "Location": "/index.html" });
+    
     response.end();
 });
 server.listen(8500); // start !
