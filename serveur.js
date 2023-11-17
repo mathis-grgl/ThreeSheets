09efcaceb4a6d6a3b39f389f32305e7b58c53535
Mathis <georgel27@etu.univ-lorraine.fr>
2023-11-17 14:47:41 +0100

    Fix serveur et package

--- serveur.js
+++ serveur.js
@@ -2,11 +2,11 @@ import { createServer } from "http";
 import fs from "fs"; // Importez le module 'fs' pour la lecture des fichiers
 
 const server = createServer((request, response) => {
-    // Vérifiez si la demande est pour la racine '/'
+    // Vérifiez si la demande est pour lanp racine '/'
     if (request.url === '/' || request.url === '/index.html') {
         
         // Utilisez fs pour lire le fichier index.html
-        fs.readFile('index.html', (err, data) => {
+        fs.readFile('html/index.html', (err, data) => {
             if (err) {
                 // En cas d'erreur, affichez un message d'erreur dans la console
                 console.error(err);
@@ -20,7 +20,7 @@ const server = createServer((request, response) => {
         });
     } else {
         // Si la demande n'est pas pour '/', redirigez vers index.html
-        response.writeHead(302, { "Location": "/index.html" });
+        response.writeHead(302, { "Location": "html/index.html" });
         response.end();
     }
 });
