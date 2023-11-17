355936b9e471a1474e669f55550ec56bcb815cb3
Boubix88 <86961375+Boubix88@users.noreply.github.com>
2023-11-17 11:37:46 +0100

    update serveur.js avec index.html

--- serveur.js
+++ serveur.js
@@ -1,10 +1,29 @@
 import { createServer } from "http";
+import fs from "fs"; // Importez le module 'fs' pour la lecture des fichiers
 
-let server = createServer((request, response) => { // paramétrage
-    // redirect to index.html
-    response.writeHead(302, {  "Location": "/index.html" });
-    
-    response.end();
+const server = createServer((request, response) => {
+    // Vérifiez si la demande est pour la racine '/'
+    if (request.url === '/' || request.url === '/index.html') {
+        // Utilisez fs pour lire le fichier index.html
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                // En cas d'erreur, affichez un message d'erreur dans la console
+                console.error(err);
+                response.writeHead(500, { 'Content-Type': 'text/plain' });
+                response.end('Internal Server Error');
+            } else {
+                // Si la lecture du fichier est réussie, renvoyez le contenu de index.html
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(data);
+            }
+        });
+    } else {
+        // Si la demande n'est pas pour '/', redirigez vers index.html
+        response.writeHead(302, { "Location": "/index.html" });
+        response.end();
+    }
 });
-server.listen(8500); // start !
-console.log("Serveur lancé.");
+
+server.listen(8500, () => {
+    console.log("Serveur lancé sur le port 8500.");
+});
